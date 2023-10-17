a1451ddcf5a04209fe66304eb621c449e06393e8
VaporSiriz <61943747+VaporSiriz@users.noreply.github.com>
2023-10-18 01:01:26 +0900

    Time: 46 ms (72.91%), Space: 18.6 MB (75.03%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -4,25 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def search(self, node: Optional[TreeNode], max_diameter: int):
-        if not node:
-            return 0, max_diameter
-        if not node.left and not node.right:
-            return 1, max_diameter
-        #print(f"{node.val}")
-        
-        left, max_diameter = self.search(node.left, max_diameter)
-        right, max_diameter = self.search(node.right, max_diameter)
-        #print(f"{node.val} left:{left}, right:{right}")
-        if max_diameter < left + right:
-            max_diameter = left + right
-        #print(f"{node.val} left:{left}, right:{right}, max_diameter:{max_diameter}")
-        
-        return left+1 if left > right else right+1, max_diameter
-        
-        
+class Solution:        
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        
-        _, max_diameter = self.search(node=root, max_diameter=0)
+        max_diameter = 0
+        def search(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+            if not node.left and not node.right:
+                return 1
+
+            left = search(node.left)
+            right = search(node.right)
+            nonlocal max_diameter
+            if max_diameter < left + right:
+                max_diameter = left + right
+
+            return 1 + max(left, right)
+
+        search(node=root)
         return max_diameter
